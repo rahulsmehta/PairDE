5827b6fa2e36cb98568a8369280079c139bfb594
Rahul Mehta <mehta.rahul95@gmail.com>
2017-03-22 16:22:54 +0530

    Basic codemirror pane (WIP)

--- editor-web-app/src/containers/App/index.tsx
+++ editor-web-app/src/containers/App/index.tsx
@@ -7,6 +7,13 @@ import Header from '../../components/Header';
 import MainSection from '../../components/MainSection';
 import * as style from './style.css';
 
+//TODO: Refactor to remove bootstrap-specific code from top level component
+import { Grid, Row, Col } from "react-bootstrap";
+import * as CodeMirror from "react-codemirror";
+import "codemirror/mode/javascript/javascript";
+import "codemirror/mode/clike/clike";
+import "codemirror/mode/python/python";
+
 interface AppProps {
   todos: TodoItemData[];
   actions: typeof TodoActions;
@@ -19,12 +26,32 @@ interface AppState {
 class App extends React.Component<AppProps, AppState>{
   render() {
     const { todos, actions, children } = this.props;
+    let opts = {
+      lineNumbers: true,
+      mode: 'text/x-java'
+    };
+    // console.log(CodeMirror);
+    console.log(CodeMirror);
     return (
-      <div className={style.normal}>
+      <div className = {style.default} >
+      <CodeMirror options = {opts} value = "public class Rahul {\n}"  />
+      </div>
+      /*<div className = {style.default} >
+        <Grid fluid>
+          <Row className="show-grid">
+            <Col md={2} className = {style.c2}>File Browser</Col>
+            <Col md={8} className = {style.c1}>
+              <CodeMirror options = {opts} value = "public class Rahul {\n}"  />
+            </Col>
+            <Col md={2} className = {style.c2}>Tools</Col>
+          </Row>
+        </Grid>
+      </div>*/
+      /*<div className={style.normal}>
         <Header addTodo={actions.addTodo} />
         <MainSection todos={todos} actions={actions} />
         {children}
-      </div>
+      </div>*/
     );
   }
 }
