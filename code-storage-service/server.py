599224fbcb1219071cb0dd1c8e05b7eecbc77fea
Sam Perkins <davidsp@princeton.edu>
2017-04-11 17:29:52 -0400

    some progress on pymongo system

--- code-storage-service/server.py
+++ code-storage-service/server.py
@@ -1,3 +1,28 @@
+from flask import Flask, request
+from flask_pymongo import PyMongo
+import json
+app = Flask(__name__)
+mongo = PyMongo(app)
+from pymongo import MongoClient
+
 """
 TODO: Implement storage service with headless git repo
 """
+
+# Set endpoint prefix to /v1 for initial API
+# app.config["APPLICATION_ROOT"] = "/v1"
+
+@app.route('/ping', methods = ['GET'])
+def pong():
+  return "pong"
+
+@app.route('/create/<pathtoresource>', methods = ['GET'])
+def create(pathtoresource):
+  client = MongoClient()
+  #newID = db.insert_one({'name':'sam'})
+  db = client.local
+  return type(db)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
