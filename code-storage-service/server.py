974b3f7f0ccc416f6e281847cc6be0773925c1e8
mike <mhw3@princeton.edu>
2017-04-23 11:22:33 -0400

    Continued working on storage and added move method

--- code-storage-service/server.py
+++ code-storage-service/server.py
@@ -16,7 +16,14 @@ def getRoot():
 	return "8013998e-160e-4b5c-aad6-7c8c203da879"
 
 def new_rid():
-   return str(uuid4())
+	i = 0
+	while i == 0:
+		rid = str(uuid4())
+		myList = list(mongo.db.code.find({'rid':rid})) 
+		if len(myList) == 0:
+			i = 1
+	return rid
+
 
 #Takes path to resource and returns RID from db
 def getRID(pathtoresource):
@@ -26,14 +33,27 @@ def getRID(pathtoresource):
 def pong():
 	return "pong"
 
+@app.route('/find/<rid>', methods=['GET'])
+def find(rid):
+	myList = list(mongo.db.code.find({'rid':rid}))
+	if len(myList) == 0:
+		return "successs"
+	else:
+		return "fuck you sperk"
+
 @app.route('/root', methods=['GET'])
 def root():
 	return "8013998e-160e-4b5c-aad6-7c8c203da879"
 
+#Takes a JSON Object with parent path/rid, file contents, and isDir boolean 
 @app.route('/create/<filename>', methods=['POST'])
 def create(filename):
 	data = json.loads(request.data)
+
 	rid = new_rid()
+
+	#STILL NEED TO DO:
+	#figure out what to do with root
 	#verify that parent is a directory
 	#verify that file with same name and parent does not exist in db
 	if data['parent'] == "":
@@ -44,16 +64,39 @@ def create(filename):
 	mongo.db.code.insert({'name':filename, 'rid':rid, 'children':[], 'contents': data['contents'], 'isDir': data['isDir'], 'parent': parent})
 	return "success"
 
+
 @app.route('/load/<pathtoresource>', methods=['GET'])
 def load(pathtoresource):
 	rid = getRID(pathtoresource)
 	myList = list(mongo.db.code.find({'rid':rid}))
 	return myList[0]['contents']
 
+
+@app.route('/move/<currentpath>/<newpath>', methods=['POST'])
+def move(currentpath, newpath):
+	rid = getRID(currentpath)
+	myList = list(mongo.db.code.find({'rid':rid}))
+
+	#delete from current parent
+	parent = myList[0]['parent']
+	mongo.db.code.update({'rid':parent}, { "$pull": {'children':rid} })
+
+	# add to new parent 
+	newrid = getRID(newpath)
+	myList = list(mongo.db.code.find({'rid':newrid}))
+	mongo.db.code.update({'rid':newrid}, { "$addToSet": {'children':rid} })
+
+	#update its parent
+	mongo.db.code.update({'rid':rid}, { "$set": {'parent':newrid} })
+
+	return "success"
+
+
 @app.route('/delete/<pathtoresource>', methods=['DELETE'])
 def delete(pathtoresource):
 	rid = getRID(pathtoresource)
 	myList = list(mongo.db.code.find({'rid':rid}))
+
 	#Remove target from parent's list of children
 	parent = myList[0]['parent']
 	mongo.db.code.update({'rid':parent}, { "$pull": {'children':rid} })
