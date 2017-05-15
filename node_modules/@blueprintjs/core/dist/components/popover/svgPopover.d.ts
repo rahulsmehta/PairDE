147a328f74a6ae377363f26286edc908898bdaa0
Rahul Mehta <mehta.rahul95@gmail.com>
2017-05-14 22:34:36 -0400

    Fuckin a

--- node_modules/@blueprintjs/core/dist/components/popover/svgPopover.d.ts
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-import { IPopoverProps } from "./popover";
-export declare class SVGPopover extends React.Component<IPopoverProps, {}> {
-    render(): JSX.Element;
-}
-export declare const SVGPopoverFactory: React.ComponentFactory<IPopoverProps & {
-    children?: React.ReactNode;
-}, SVGPopover>;
--- node_modules/@blueprintjs/core/dist/components/tooltip/svgTooltip.d.ts
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-import { ITooltipProps } from "./tooltip";
-export declare class SVGTooltip extends React.Component<ITooltipProps, {}> {
-    render(): JSX.Element;
-}
-export declare const SVGTooltipFactory: React.ComponentFactory<ITooltipProps & {
-    children?: React.ReactNode;
-}, SVGTooltip>;
